3b6a1dfc09bf045e93884c6f666d9542ca054d44
daniyaF19 <mitad6391@gmail.com>
2020-03-02 20:23:12 +0530

    array asssignment done

---
+++ index.js
@@ -0,0 +1,77 @@
+// var a=10;
+// var b=true;
+// var c;
+// var d='daniya';
+// var e=null;
+
+// console.log(typeof(a));
+
+// //function
+// function add (a,b) {
+//     return a+b;
+// }
+
+// console.log(add(3,5));
+
+// function isNumberPositive(a) {
+//     if(a<0){
+//         console.log('a is negative');
+//     } else if(a==0){
+//         console.log('a is zero');
+//     } else{
+//         console.log('a is positive')
+//     }
+    
+// }
+// console.log(isNumberPositive(0));
+
+// function fn(a,b){
+//     if(a<b){
+//         console.log('a is lesser');
+//     }else if(a>b){
+//         console.log('b is greater');
+//     }else{
+// console.log('equal');
+//     }
+// }
+
+//  var result=""
+//  var i=-2;
+//  do{
+//      i=i+1;
+//      result = result+i;
+//      console.log("the value is",i);
+     
+// }while(i<5);
+// console.log(result);
+
+// function ChangeName() {
+//     alert(ChangeName)
+// }
+
+// function ChangeName(){
+//     var domElement=document.getElementById("name")
+//     domElement.innerHTML="CSS IS BEST"
+// }
+// var a;
+// function sub(c,d) {
+//     var e= c-d;
+//     console.log(e);
+//     return e;
+    
+//     }
+//     console.log(sub(5,3));
+ 
+var arr = [1,2,3,4,5]
+console.log("array numbers r:");
+function numbers(){
+    for(var i=0;i<=arr.length;i++)
+    console.log(arr[i]+ "");
+    
+}
+numbers();
+console.log("numbers");
+
+
+   
+
